f3112109d5ec813062788faf0762cfca5381a960
Marie-Laure <58740748+mlriedinger@users.noreply.github.com>
2020-09-25 07:27:31 +0200

    Update overloading.php

--- overloading.php
+++ overloading.php
@@ -2,11 +2,21 @@
 
 class Message {
 
-    public function displayMessage($word) {
+    function __call($name, $argument){
+    if($name == 'displayMessage') {
+        swicth(count($argument)){
+            case 0 : return 0;
+            case 1 : return displayMessage1($argument[0]);
+            case 2 : return displayMessage2($argument[0], $argument[1]);
+        }
+    }
+}
+
+    public function displayMessage1($word) {
         echo '<p>Ma fonction avec un paramètre : ' . $word . '</p>';
     }
 
-    public function displayMessage($word1, $word2) {
+    public function displayMessage2($word1, $word2) {
         echo '</p>Ma fonction avec deux paramètres : ' . $word1 . ' ' . $word2 . '</p>';
     }
 }
